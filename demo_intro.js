b1a4d9113c3bbb1da427136850299ba06bfad462
Arvind vyas <arvindvyas07@gmail.com>
2024-01-07 16:17:17 +0530

    Added example of open file in which it will print 'the file is open' into the console

--- demo_intro.js
+++ demo_intro.js
@@ -4,6 +4,12 @@ var http = require('http');
 
 // require module
 var dt = require('./myfirstmodule');
+var fs = require('fs');
+var rs = fs.createReadStream('./demofile.txt');
+  
+rs.on('open', function() {
+	console.log('the file is open');
+});
 
 http.createServer(function(req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plan'});
