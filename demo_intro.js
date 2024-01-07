13bf89b29f2554d3dd41a63e2b9486501aac4a2c
Arvind vyas <arvindvyas07@gmail.com>
2024-01-07 16:25:18 +0530

    Added example of event emitter

--- demo_intro.js
+++ demo_intro.js
@@ -5,8 +5,30 @@ var http = require('http');
 // require module
 var dt = require('./myfirstmodule');
 var fs = require('fs');
+var events = require('events');
+
+
+// create object
+var eventEmitter = new events.EventEmitter();
+
 var rs = fs.createReadStream('./demofile.txt');
   
+
+//Create an event handler:
+
+var myEventHandler = function() {
+	console.log('I hear a scream');
+}
+
+// Assign the event handler to an event
+eventEmitter.on('scream', myEventHandler)
+
+
+//Fire the 'scream' event:
+eventEmitter.emit('scream');
+
+
+// create the fuction for file open
 rs.on('open', function() {
 	console.log('the file is open');
 });
